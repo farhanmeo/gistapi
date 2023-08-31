b8f5ebab9518ee2b7813045da4a00dd980b3e0f2
farhanmeo <farhanmeo2@gmail.com>
2023-08-31 16:23:59 +0500

    Gist UI and rendering

--- src/components/GistList.js
+++ src/components/GistList.js
@@ -1,4 +1,43 @@
-import React, { useEffect, useState } from 'react'
-const GistList = () => <></>
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGists } from '../actions/gistActions';
+import Gist from './Gist';
+import './GistList.css';
 
-export default GistList
+const GistList = ({ searchTerm }) => {
+  const dispatch = useDispatch();
+  const allGists = useSelector(state => state.gists.gists);
+  const [filteredGists, setFilteredGists] = useState(allGists);
+
+  useEffect(() => {
+    dispatch(fetchGists()); // Fetch all gists by default
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (searchTerm) {
+      const filtered = allGists.filter(gist =>
+        gist.owner.login.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredGists(filtered);
+    } else {
+      setFilteredGists(allGists); // Show all gists if no search term
+    }
+  }, [allGists, searchTerm]);
+console.log("filteredGists", filteredGists);
+  return (
+    <div>
+      {
+      filteredGists.length > 0 ?
+      filteredGists.map(gist => (
+        <Gist key={gist.id} gist={gist} />
+      )):
+      <div  className="gist-list no-data">
+<img src="/assets/noRecords.png" alt="No Data" className="no-data-image" />
+      {/* <h1>No Record Found</h1> */}
+      </div>
+      }
+    </div>
+  );
+};
+
+export default GistList;
