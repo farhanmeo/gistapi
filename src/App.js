9e30dee56f9f73d0171399e075f62ed98dd9a035
farhanmeo <farhanmeo2@gmail.com>
2023-08-31 16:23:41 +0500

    Provide and handleSearch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 
+import { useState } from 'react';
+import { Provider } from 'react-redux';
 import styled from 'styled-components'
+import GistList from './components/GistList';
 import Header from "./components/Header";
 import GlobalStyles from "./GlobalStyle";
+import store from './store/store';
 
 const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+  };
+
   return (
+    <Provider store={store}>
     <Wrapper className="App" data-testid="app">
-      <Header />
+    <Header onSearch={handleSearch} />
+      <GistList searchTerm={searchTerm} />
       <GlobalStyles />
     </Wrapper>
+    </Provider>
   );
 }
 
